ed51010a01950d612bfa956c81b3dd907516a8a0
andre.av <andre.av@pf.gov.br>
2024-06-01 13:18:43 -0300

    feat: address modeling

---
+++ prodemge-fase3/app/Models/Adress.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Adress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'people_id',
+        'business',
+        'postal_code',
+        'number',
+        'complement',
+        'neighborhood',
+        'uf',
+        'city'
+    ];
+}
--- prodemge-fase3/app/Models/People.php
+++ prodemge-fase3/app/Models/People.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class People extends Model
@@ -28,5 +29,14 @@ class People extends Model
     public function user(): HasOne
     {
         return $this->hasOne(User::class,'id','user_id');
+    }
+    /**
+     * Get all of the address for the People
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function address(): HasMany
+    {
+        return $this->hasMany(Adress::class, 'people_id', 'id');
     }
 }
